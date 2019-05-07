5c7c446e84c8bbf6dd77cf38c7dfa0c5c0118cdf
PhucDL <danglephuc@gmail.com>
2019-05-07 09:48:14 +0700

    Add project files.

---
+++ PManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
